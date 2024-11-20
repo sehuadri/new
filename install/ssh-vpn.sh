e553dbb8b8e8c6de9e362166b68e8e0f111870ab
sehuadri <sehuadri@gmail.com>
2024-11-21 04:43:59 +0700

    Update ssh-vpn.sh

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -196,12 +196,12 @@ echo "/usr/sbin/nologin" >> /etc/shells
 /etc/init.d/dropbear restart
 
 # // install squid for debian 9,10 & ubuntu 20.04
-apt -y install squid3
+#apt -y install squid3
 
 # install squid for debian 11
-apt -y install squid
+#apt -y install squid
 #wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/sehuadri/new/main/install/squid3.conf"
-sed -i $MYIP2 /etc/squid/squid.conf
+#sed -i $MYIP2 /etc/squid/squid.conf
 
 # setting vnstat
 #apt -y install vnstat
