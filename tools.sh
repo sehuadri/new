94b331047c2c51b5ddae7f81f700c33975339ab8
sehuadri <sehuadri@gmail.com>
2024-11-21 16:45:26 +0700

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -19,27 +19,25 @@ echo "Tools install...!"
 echo "Progress..."
 sleep 0.5
 
-  apt update -y
-  apt upgrade -y
-  apt dist-upgrade -y
-  apt install netfilter-persistent -y
-  apt-get remove --purge ufw firewalld -y 
-  apt-get remove --purge exim4 -y 
+sudo apt update -y
+sudo apt update -y
+sudo apt dist-upgrade -y
+sudo apt install netfilter-persistent -y
+sudo apt-get remove --purge ufw firewalld -y 
+sudo apt-get remove --purge exim4 -y 
 
 
-  apt install -y screen curl jq bzip2 gzip coreutils rsyslog iftop \
+sudo apt install -y screen curl jq bzip2 gzip coreutils rsyslog iftop \
  htop zip unzip net-tools sed gnupg gnupg1 \
- bc   apt-transport-https build-essential dirmngr libxml-parser-perl neofetch screenfetch git lsof \
+ bc sudo apt-transport-https build-essential dirmngr libxml-parser-perl neofetch screenfetch git lsof \
  openssl openvpn easy-rsa fail2ban tmux \
  stunnel4 vnstat squid3 \
  dropbear  libsqlite3-dev \
- socat cron bash-completion ntpdate xz-utils   apt-transport-https \
+ socat cron bash-completion ntpdate xz-utils sudo apt-transport-https \
  gnupg2 dnsutils lsb-release chrony
 
-curl -sSL https://deb.nodesource.com/setup_23.x | bash - 
-  apt-get install nodejs -y
-  apt -y install vnstat
-/etc/init.d/vnstat restart
+curl -sSL https://deb.nodesource.com/setup_20.x | bash - 
+sudo apt-get install nodejs -y
 wget -q https://humdi.net/vnstat/vnstat-2.6.tar.gz
 tar zxvf vnstat-2.6.tar.gz
 cd vnstat-2.6
@@ -53,7 +51,7 @@ systemctl enable vnstat
 rm -f /root/vnstat-2.6.tar.gz >/dev/null 2>&1
 rm -rf /root/vnstat-2.6 >/dev/null 2>&1
 
-  apt install -y libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev xl2tpd pptpd
+sudo apt install -y libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev xl2tpd pptpd
 
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 yellow "Dependencies successfully installed..."
