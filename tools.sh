b218e4e979383ccacb1f0ba1185ba90a5a0824d8
sehuadri <sehuadri@gmail.com>
2024-11-21 17:35:12 +0700

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -17,43 +17,45 @@ fi
 
 echo "Tools install...!"
 echo "Progress..."
-sleep 0.5
+sleep 2
 
-sudo apt update -y
-sudo apt update -y
-sudo apt dist-upgrade -y
-sudo apt install netfilter-persistent -y
-sudo apt-get remove --purge ufw firewalld -y 
-sudo apt-get remove --purge exim4 -y 
+apt update -y
+apt upgrade -y
+apt dist-upgrade -y
+apt install sudo -y
+apt-get remove --purge ufw firewalld -y 
+apt-get remove --purge exim4 -y 
+apt-get remove apache2 -y 
 
-
-sudo apt install -y screen curl jq bzip2 gzip coreutils rsyslog iftop \
- htop zip unzip net-tools sed gnupg gnupg1 \
- bc sudo apt-transport-https build-essential dirmngr libxml-parser-perl neofetch screenfetch git lsof \
- openssl openvpn easy-rsa fail2ban tmux \
- stunnel4 vnstat squid3 \
- dropbear  libsqlite3-dev \
- socat cron bash-completion ntpdate xz-utils sudo apt-transport-https \
- gnupg2 dnsutils lsb-release chrony
+apt install -y screen curl jq bzip2 gzip coreutils rsyslog iftop \
+htop zip unzip net-tools sed gnupg gnupg1 \
+bc sudo apt-transport-https build-essential dirmngr libxml-parser-perl neofetch screenfetch git lsof \
+openssl openvpn easy-rsa fail2ban tmux \
+stunnel4 vnstat squid3 \
+dropbear  libsqlite3-dev \
+socat cron bash-completion ntpdate xz-utils sudo apt-transport-https \
+gnupg2 dnsutils lsb-release chrony
 
 curl -sSL https://deb.nodesource.com/setup_23.x | bash - 
-sudo apt-get install nodejs -y
-wget -q https://humdi.net/vnstat/vnstat-2.6.tar.gz
+apt-get install nodejs -y
+
+apt -y install libsqlite3-dev
+wget https://raw.githubusercontent.com/sehuadri/new/main/vnstat-2.6.tar.gz
 tar zxvf vnstat-2.6.tar.gz
 cd vnstat-2.6
-./configure --prefix=/usr --sysconfdir=/etc >/dev/null 2>&1 && make >/dev/null 2>&1 && make install >/dev/null 2>&1
+./configure --prefix=/usr --sysconfdir=/etc && make && make install
 cd
 vnstat -u -i $NET
 sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
 chown vnstat:vnstat /var/lib/vnstat -R
 systemctl enable vnstat
 /etc/init.d/vnstat restart
-rm -f /root/vnstat-2.6.tar.gz >/dev/null 2>&1
-rm -rf /root/vnstat-2.6 >/dev/null 2>&1
+rm -f /root/vnstat-2.6.tar.gz
+rm -rf /root/vnstat-2.6
 
-sudo apt install -y libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev xl2tpd pptpd
+apt install -y libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev xl2tpd pptpd
 
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 yellow "Dependencies successfully installed..."
-sleep 1
+sleep 3
 clear
