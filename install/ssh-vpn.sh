3f378b16ca1f4a76d9b7ff575c1d9d80ccbd267f
sehuadri <sehuadri@gmail.com>
2024-11-21 22:42:27 +0700

    Update ssh-vpn.sh

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -378,14 +378,14 @@ service cron reload >/dev/null 2>&1
 service cron start >/dev/null 2>&1
 
 # remove unnecessary files
-sleep 1
-echo -e "[ ${green}INFO$NC ] Clearing trash"
 apt autoclean -y >/dev/null 2>&1
-
-if dpkg -s unscd >/dev/null 2>&1; then
 apt -y remove --purge unscd >/dev/null 2>&1
-fi  
-
+apt-get -y --purge remove samba* >/dev/null 2>&1
+apt-get -y --purge remove apache2* >/dev/null 2>&1
+apt-get -y --purge remove bind9* >/dev/null 2>&1
+apt-get -y remove sendmail* >/dev/null 2>&1
+apt autoremove -y >/dev/null 2>&1
+# finishing
 cd
 chown -R www-data:www-data /home/vps/public_html
 
@@ -393,5 +393,6 @@ rm -f /root/key.pem
 rm -f /root/cert.pem
 rm -f /root/ssh-vpn.sh
 rm -f /root/bbr.sh
+rm -rf /etc/apache2
 
 clear
