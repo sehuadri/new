b8f9e8a82d399acd3f2fd765273fe59e64a1e5f7
sehuadri <sehuadri@gmail.com>
2024-12-01 22:56:23 +0700

    Update udp-custom.sh

--- install/udp-custom.sh
+++ install/udp-custom.sh
@@ -10,12 +10,10 @@ ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
 # install udp-custom
 echo downloading udp-custom
-wget -q --show-progress --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ixz82G_ruRBnEEp4vLPNF2KZ1k8UfrkV' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ixz82G_ruRBnEEp4vLPNF2KZ1k8UfrkV" -O /etc/udp/udp-custom && rm -rf /tmp/cookies.txt
-chmod +x /etc/udp/udp-custom
+wget -q -O /etc/udp/udp-custom "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/udp/udp-custom-linux-amd64" && chmod +x /etc/udp/udp-custom
 
 echo downloading default config
-wget -q --show-progress --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1klXTiKGUd2Cs5cBnH3eK2Q1w50Yx3jbf' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1klXTiKGUd2Cs5cBnH3eK2Q1w50Yx3jbf" -O /etc/udp/config.json && rm -rf /tmp/cookies.txt
-chmod 644 /etc/udp/config.json
+wget -q -O /etc/udp/config.json "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/udp/config.json" && chmod 644 /etc/udp/config.json
 
 if [ -z "$1" ]; then
 cat <<EOF > /etc/systemd/system/udp-custom.service
