ac64dd8eb4e27365074b2e62b349634b0906a594
sehuadri <sehuadri@gmail.com>
2024-12-04 20:33:34 +0700

    Update udp-custom.sh

--- install/udp-custom.sh
+++ install/udp-custom.sh
@@ -10,10 +10,10 @@ ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
 # install udp-custom
 echo downloading udp-custom
-wget -q -O /etc/udp/udp-custom "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/udp/udp-custom-linux-amd64" && chmod +x /etc/udp/udp-custom
+wget -q -O /etc/udp/udp-custom "https://raw.githubusercontent.com/sehuadri/new/main/udp-custom/udp-custom-linux-amd64" && chmod +x /etc/udp/udp-custom
 
 echo downloading default config
-wget -q -O /etc/udp/config.json "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/udp/config.json" && chmod 644 /etc/udp/config.json
+wget -q -O /etc/udp/config.json "https://raw.githubusercontent.com/sehuadri/new/main/udp-custom/config.json" && chmod 644 /etc/udp/config.json
 
 if [ -z "$1" ]; then
 cat <<EOF > /etc/systemd/system/udp-custom.service
