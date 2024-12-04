f61ac0e41b071f3351e58a9ab656b3fae42f23cd
sehuadri <sehuadri@gmail.com>
2024-12-04 21:12:34 +0700

    Update udp-custom.sh

--- install/udp-custom.sh
+++ install/udp-custom.sh
@@ -10,7 +10,8 @@ ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
 # install udp-custom
 echo downloading udp-custom
-wget -q -O /etc/udp/udp-custom "https://raw.githubusercontent.com/sehuadri/new/main/udp/udp-custom-linux-amd64" && chmod +x /etc/udp/udp-custom
+wget -q --show-progress --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ixz82G_ruRBnEEp4vLPNF2KZ1k8UfrkV' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ixz82G_ruRBnEEp4vLPNF2KZ1k8UfrkV" -O /etc/udp/udp-custom && rm -rf /tmp/cookies.txt
+chmod +x /etc/udp/udp-custom
 
 echo downloading default config
 wget -q -O /etc/udp/config.json "https://raw.githubusercontent.com/sehuadri/new/main/udp/config.json" && chmod 644 /etc/udp/config.json
