22515ec1872e0dcdce3f51a56e9141d109b37c54
sehuadri <sehuadri@gmail.com>
2025-01-08 20:07:54 +0700

    Update ovpn.py

--- sshws/ovpn.py
+++ sshws/ovpn.py
@@ -3,19 +3,16 @@ import socket, threading, thread, select, signal, sys, time, getopt
 
 # Listen
 LISTENING_ADDR = '0.0.0.0'
-if sys.argv[1:]:
-  LISTENING_PORT = sys.argv[1]
-else:
-  LISTENING_PORT = 2096  
-#Pass
+LISTENING_PORT = sys.argv[1]
+
+# Pass
 PASS = ''
 
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:1194'
-RESPONSE = 'HTTP/1.1 101 <u><font color="#00ff00">Switching Protocol</font></u>(Openvpn)\r\nContent-Length: 104857600000\r\n\r\n'
-#RESPONSE = 'HTTP/1.1 200 Websocket_openvpn\r\nContent-length: 0\r\nHTTP/1.1 200 Connection established\r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 Switching Protocols\r\n\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
