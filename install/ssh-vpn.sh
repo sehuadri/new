e7b090895fe4f5404f3d2acacda764e3ef99cb34
sehuadri <sehuadri@gmail.com>
2025-02-03 22:11:33 +0700

    Update ssh-vpn.sh

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -288,7 +288,7 @@ sudo apt-get install tcpdump -y
 sudo apt-get install dsniff -y
 sudo apt install grepcidr -y
 
-wget https://github.com/jgmdev/ddos-deflate/archive/master.zip -O ddos.zip
+wget https://github.com/sehuadri/ddos-deflate/archive/master.zip -O ddos.zip
 unzip ddos.zip
 cd ddos-deflate-master
 ./install.sh
