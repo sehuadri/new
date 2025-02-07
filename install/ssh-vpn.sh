85f2f4386a710b54760c8f66f92101ad787c9335
sehuadri <sehuadri@gmail.com>
2025-02-07 20:28:27 +0700

    Update ssh-vpn.sh

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -265,13 +265,13 @@ wget https://raw.githubusercontent.com/sehuadri/new/main/install/vpn.sh &&  chmo
 wget https://raw.githubusercontent.com/sehuadri/new/main/install/lolcat.sh &&  chmod +x lolcat.sh && ./lolcat.sh
 
 # memory swap 2gb
-cd
-dd if=/dev/zero of=/swapfile bs=2048 count=1048576
-mkswap /swapfile
-chown root:root /swapfile
-chmod 0600 /swapfile >/dev/null 2>&1
-swapon /swapfile >/dev/null 2>&1
-sed -i '$ i\/swapfile      swap swap   defaults    0 0' /etc/fstab
+#cd
+#dd if=/dev/zero of=/swapfile bs=2048 count=1048576
+#mkswap /swapfile
+#chown root:root /swapfile
+#chmod 0600 /swapfile >/dev/null 2>&1
+#swapon /swapfile >/dev/null 2>&1
+#sed -i '$ i\/swapfile      swap swap   defaults    0 0' /etc/fstab
 
 
 # > Singkronisasi jam
