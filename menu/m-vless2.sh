17705075ce94832b58de40fd8862e84804a4894c
sehuadri <sehuadri@gmail.com>
2025-02-07 22:17:28 +0700

    Update m-vless2.sh

--- menu/m-vless2.sh
+++ menu/m-vless2.sh
@@ -66,7 +66,22 @@ read -n 1 -s -r -p "Press any key to back"
 add-vless
 fi
 done
-uuid=$(cat /proc/sys/kernel/random/uuid)
+#uuid=$(cat /proc/sys/kernel/random/uuid)
+read -p " CREAT PW (OTOMATIC RANDOM PW) :" uuid
+    [[ -z "$uuid" ]] && uuid=$(cat /proc/sys/kernel/random/uuid)
+sec=3
+echo ""
+spinner=(⣻ ⢿ ⡿ ⣟ ⣯ ⣷)
+while [ $sec -gt 0 ]; do
+  echo -ne "\e[33m ${spinner[sec]} Setting up a Premium Account $sec seconds...\r"
+  sleep 1
+  sec=$(($sec - 1))
+done
+clear
+echo ""
+echo -e "\e[1;32m   input dependecies account $user\e[0m\n"
+echo -e "\033[0;33m .::.  Script by CLOUDVPN  .::.\e[0m\n"
+echo ""
 until [[ $masaaktif =~ ^[0-9]+$ ]]; do
 read -p "Expired (hari): " masaaktif
 done
