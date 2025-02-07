c70446d44a287d6559433e9a7e48e861cb4a4d77
sehuadri <sehuadri@gmail.com>
2025-02-07 09:23:28 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -1,64 +1,15 @@
-#!/bin/bash
-#installer Websocker tunneling 
-url="https://raw.githubusercontent.com/sehuadri/new/main"
-cd
-
-#Install Script Websocket-SSH Python
-wget -O /usr/local/bin/edu-proxy ${url}/sshws/https.py
-wget -O /usr/local/bin/ws-dropbear ${url}/sshws/dropbear-ws.py.txt
-wget -O /usr/local/bin/ws-stunnel ${url}/sshws/ws-stunnel.txt
-#wget -O /usr/local/bin/edu-proxyovpn ${url}/sshws/ovpn.py
-#wget -O /usr/local/bin/ws-openssh ${url}/sshws/ws-openssh
-
-#izin permision
-chmod +x /usr/local/bin/edu-proxy
-chmod +x /usr/local/bin/ws-dropbear
-chmod +x /usr/local/bin/ws-stunnel
-#chmod +x /usr/local/bin/edu-proxyovpn
-#chmod +x /usr/local/bin/ws-openssh
-
-#System Direcly dropbear Websocket-SSH Python
-wget -O /etc/systemd/system/edu-proxy.service ${url}/sshws/http.service && chmod +x /etc/systemd/system/edu-proxy.service
-
-#System Dropbear Websocket-SSH Python
-wget -O /etc/systemd/system/ws-dropbear.service ${url}/sshws/service-wsdropbear.txt && chmod +x /etc/systemd/system/ws-dropbear.service
-
-#System SSL/TLS Websocket-SSH Python
-wget -O /etc/systemd/system/ws-stunnel.service ${url}/sshws/ws-stunnel.service.txt && chmod +x /etc/systemd/system/ws-stunnel.service
-
-#System Websocket-OpenVPN Python
-#wget -O /etc/systemd/system/edu-proxyovpn.service ${url}/sshws/edu-proxyovpn.service && chmod +x /etc/systemd/system/edu-proxyovpn.service
-
-#System Websocket-Openssh
-#wget -O /etc/systemd/system/ws-openssh.service ${url}/sshws/ws-openssh.service && chmod +x /etc/systemd/system/ws-openssh.service
-
-
-#restart service
-systemctl daemon-reload
-
-#Enable & Start & Restart directly dropbear
-systemctl daemon-reload
-systemctl enable edu-proxy.service
-systemctl start edu-proxy.service
-systemctl restart edu-proxy.service
-
-#Enable & Start & Restart ws-dropbear service
-systemctl enable ws-dropbear.service
-systemctl start ws-dropbear.service
-systemctl restart ws-dropbear.service
-
-#Enable & Start & Restart ws-stunnel service
-systemctl enable ws-stunnel.service
-systemctl start ws-stunnel.service
-systemctl restart ws-stunnel.service
-
-#systemctl daemon-reload
-#systemctl enable edu-proxyovpn.service
-#systemctl start edu-proxyovpn.service
-#systemctl restart edu-proxyovpn.service
-
-#Enable & Start & Restart ws-openssh service
-#systemctl enable ws-openssh.service
-#systemctl start ws-openssh.service
-#systemctl restart ws-openssh.service
-clear
+wget -O /usr/bin/ws "${REPO}ws/ws" >/dev/null 2>&1
+    wget -O /usr/bin/tun.conf "${REPO}ws/tun.conf" >/dev/null 2>&1
+    wget -O /etc/systemd/system/ws.service "${REPO}ws/ws.service" >/dev/null 2>&1
+    chmod +x /etc/systemd/system/ws.service
+    chmod +x /usr/bin/ws
+    chmod 644 /usr/bin/tun.conf
+systemctl disable ws
+systemctl stop ws
+systemctl enable ws
+systemctl start ws
+systemctl restart ws
+wget -q -O /usr/local/share/xray/geosite.dat "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat" >/dev/null 2>&1
+wget -q -O /usr/local/share/xray/geoip.dat "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat" >/dev/null 2>&1
+wget -O /usr/sbin/ftvpn "${REPO}ws/ftvpn" >/dev/null 2>&1
+chmod +x /usr/sbin/ftvpn
