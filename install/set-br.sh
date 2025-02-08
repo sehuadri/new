e0c8ca7377ce86ebb26129fdd796b8a08ddc660e
sehuadri <sehuadri@gmail.com>
2025-02-08 08:24:24 +0700

    Update set-br.sh

--- install/set-br.sh
+++ install/set-br.sh
@@ -16,36 +16,6 @@ chmod +x /usr/bin/backup
 chmod +x /usr/bin/restore
 chmod +x /usr/bin/xp
 cd
-#if [ ! -f "/etc/cron.d/cleaner_otm" ]; then
-cat> /etc/cron.d/cleaner << END
-SHELL=/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-*/2 * * * * root /usr/bin/cleaner
-END
-#fi
-
-
-
-#if [ ! -f "/etc/cron.d/xp_otm" ]; then
-cat> /etc/cron.d/xp_otm << END
-SHELL=/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-0 0 * * * root /usr/bin/xp
-END
-#fi
-cat > /home/re_otm <<-END
-7
-END
-
-#if [ ! -f "/etc/cron.d/bckp_otm" ]; then
-cat> /etc/cron.d/bckp_otm << END
-SHELL=/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-0 5 * * * root /usr/bin/bottelegram
-END
-#fi
-
-service cron restart > /dev/null 2>&1
 
 # > Pasang Limit
 
