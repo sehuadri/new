1881a81564e8600f09dc63f24dfacfa5d5504e24
sehuadri <sehuadri@gmail.com>
2025-02-08 18:56:39 +0700

    Update ssh-vpn.sh

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -379,7 +379,7 @@ END
 cat> /etc/cron.d/autocpu << END
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-*/1 * * * * root /usr/bin/autocpu
+0 * * * * root /usr/bin/autocpu
 END
 #fi
 
@@ -387,7 +387,7 @@ END
 cat> /etc/cron.d/notramcpu << END
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-0 */6 * * * root /usr/bin/notramcpu
+0 */2 * * * root /usr/bin/notramcpu
 #fi
 
 cat> /etc/cron.d/tendang << END
@@ -416,7 +416,7 @@ END
 cat> /etc/cron.d/clearlog << END
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-0 */6 * * * root /usr/bin/clearlog
+0 */3 * * * root /usr/bin/clearlog
 END
 
 cat >/etc/cron.d/daily_reboot <<-END
