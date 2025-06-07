7f07e016702e3f031199e4f3e38f9081030adb57
sehuadri <sehuadri@gmail.com>
2025-06-07 20:11:14 +0700

    Update ssh-vpn.sh

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -379,7 +379,8 @@ END
 cat> /etc/cron.d/autocpu << END
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-*/1 * * * * root /usr/bin/autocpuEND
+*/1 * * * * root /usr/bin/autocpu
+END
 #fi
 
 #if [ ! -f "/etc/cron.d/notramcpu" ]; then
