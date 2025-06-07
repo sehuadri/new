e7523ceb741d7a21e5fa3702c92e9aa738b41c48
sehuadri <sehuadri@gmail.com>
2025-06-07 18:02:26 +0700

    Update rmbl.sh

--- install/rmbl.sh
+++ install/rmbl.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 apt install jq curl -y
-DOMAIN=satanfusion.cloud
+DOMAIN=cloudvpn.shop
 sub=$(cat /root/subdomainx)
-dns=${sub}.satanfusion.cloud
-dns2=*.${sub}.satanfusion.cloud
-CF_ID=bangtepllo752@gmail.com
-CF_KEY=d8e5c652e1ddcc6fbfb20b1d7b6364de70c82
+dns=${sub}.cloudvpn.shop
+dns2=*.${sub}.cloudvpn.shop
+CF_ID=qwqw34207@gmail.com
+CF_KEY=266a89fba5c8824b989d663b382ba84f06d17
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${dns}..."
@@ -43,12 +43,12 @@ echo $dns > /root/domain
 echo "$dns" > /etc/xray/domain
 echo "$dn!������MYIP=$(wget -qO- icanhazip.com);
 apt install jq curl -y
-DOMAIN=satanfusion.cloud
+DOMAIN=cloudvpn.shop
 sub=$(cat /root/subdomainx)
-dns=${sub}.satanfusion.cloud
-dns2=*.${sub}.satanfusion.cloud
-CF_ID=bangtepllo752@gmail.com
-CF_KEY=d8e5c652e1ddcc6fbfb20b1d7b6364de70c82
+dns=${sub}.cloudvpn.shop
+dns2=*.${sub}.cloudvpn.shop
+CF_ID=qwqw34207@gmail.com
+CF_KEY=266a89fba5c8824b989d663b382ba84f06d17
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${dns}..."
