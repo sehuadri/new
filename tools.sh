09b7068e3ba8651dc9ca262059493dd32377da2e
sehuadri <sehuadri@gmail.com>
2025-06-07 10:17:33 +0700

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -8,50 +8,61 @@ green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
 
+#!/bin/bash
+
 echo "           Tools install...!"
 echo "                  Progress..."
-sleep 0.5
-apt update -y
-apt upgrade -y
-apt dist-upgrade -y
+sleep 0.1
+
+# Update dan upgrade sistem
+apt update -y && apt upgrade -y && apt dist-upgrade -y
 apt install sudo -y
+
+# Bersihkan cache apt
 sudo apt-get clean all
-apt install -y debconf-utils
-apt-get remove --purge ufw firewalld -y
-apt-get remove --purge exim4 -y
+
+# Instalasi utilitas penting
+apt install -y debconf-utils haproxy p7zip-full software-properties-common --no-install-recommends
+
+# Hapus firewall bawaan yang tidak dibutuhkan
+apt-get remove --purge -y ufw firewalld exim4
+
+# Bersihkan paket tidak terpakai
 apt-get autoremove -y
-apt install -y --no-install-recommends software-properties-common
+
+# Konfigurasi iptables agar autosave aktif
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install iptables iptables-persistent netfilter-persistent figlet ruby libxml-parser-perl squid nmap screen curl jq bzip2 gzip coreutils rsyslog iftop htop zip unzip net-tools sed gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch screenfetch lsof openssl openvpn easy-rsa fail2ban tmux stunnel4 squid3 dropbear socat cron bash-completion ntpdate xz-utils apt-transport-https gnupg2 dnsutils lsb-release chrony libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev xl2tpd pptpd apt git speedtest-cli p7zip-full libjpeg-dev zlib1g-dev python python3 python3-pip shc build-essential nodejs nginx php php-fpm php-cli php-mysql p7zip-full
 
-# remove unnecessary files
+# Instalasi paket tambahan
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install \
+  iptables iptables-persistent netfilter-persistent figlet ruby libxml-parser-perl \
+  squid nmap screen curl jq bzip2 gzip coreutils rsyslog iftop htop zip unzip net-tools \
+  sed gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl \
+  neofetch screenfetch lsof openssl openvpn easy-rsa fail2ban tmux stunnel4 squid \
+  dropbear socat cron bash-completion ntpdate xz-utils apt-transport-https gnupg2 \
+  dnsutils lsb-release chrony libnss3-dev libnspr4-dev pkg-config libpam0g-dev \
+  libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-openssl-dev flex bison make \
+  libnss3-tools libevent-dev xl2tpd apt git speedtest-cli p7zip-full libjpeg-dev \
+  zlib1g-dev python-is-python3 python3-pip shc build-essential nodejs nginx php \
+  php-fpm php-cli php-mysql p7zip-full squid libcurl4-openssl-dev
+
+# Instalasi gotop
+gotop_latest="$(curl -s https://api.github.com/repos/xxxserxxx/gotop/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
+gotop_link="https://github.com/xxxserxxx/gotop/releases/download/v$gotop_latest/gotop_v"$gotop_latest"_linux_amd64.deb"
+curl -sL "$gotop_link" -o /tmp/gotop.deb
+dpkg -i /tmp/gotop.deb
+
+# Bersihkan file yang tidak diperlukan
 sudo apt-get autoclean -y >/dev/null 2>&1
-audo apt-get -y --purge removd unscd >/dev/null 2>&1
-sudo apt-get -y --purge remove samba* >/dev/null 2>&1
-sudo apt-get -y --purge remove apache2* >/dev/null 2>&1
-sudo apt-get -y --purge remove bind9* >/dev/null 2>&1
-sudo apt-get -y remove sendmail* >/dev/null 2>&1
+sudo apt-get -y --purge remove \
+  samba* apache2* bind9* sendmail* unscd >/dev/null 2>&1
 apt autoremove -y >/dev/null 2>&1
-# finishing
 
-sudo apt-get -y install vnstat
-/etc/init.d/vnstat restart
-sudo apt-get -y install libsqlite3-dev
-wget https://raw.githubusercontent.com/sehuadri/new/main/main/vnstat-2.6.tar.gz
-tar zxvf vnstat-2.6.tar.gz
-cd vnstat-2.6
-./configure --prefix=/usr --sysconfdir=/etc && make && make install
-cd
-vnstat -u -i $NET
-sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
-chown vnstat:vnstat /var/lib/vnstat -R
-systemctl enable vnstat
-/etc/init.d/vnstat restart
-rm -f /root/vnstat-2.6.tar.gz
-rm -rf /root/vnstat-2.6
+echo "           Instalasi selesai!"
+
 
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 yellow "Dependencies successfully installed..."
-sleep 1
 clear
+rm -r tools.sh
